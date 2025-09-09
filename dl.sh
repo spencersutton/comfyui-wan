a1ca247766dfded8231192862df96bba88034fbb
Spencer Sutton <spencer.e.sutton@gmail.com>
2025-09-09 13:56:27 -0400

    dl

--- dl.sh
+++ dl.sh
@@ -1,3 +1,12 @@
+echo "Building SageAttention in the background"
+(
+	git clone https://github.com/thu-ml/SageAttention.git
+	cd SageAttention || exit 1
+	python3 setup.py install
+	cd /
+	pip install --no-cache-dir triton
+) &>/var/log/sage_build.log & # run in background, log output
+
 wget -O /workspace/ComfyUI/models/diffusion_models/wan2.2_i2v_high_noise_14B_fp16.safetensors https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_i2v_high_noise_14B_fp16.safetensors
 wget -O /workspace/ComfyUI/models/diffusion_models/wan2.2_i2v_low_noise_14B_fp16.safetensors https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_i2v_low_noise_14B_fp16.safetensors
 
